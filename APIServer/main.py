be0d1260e38488776fd52822f1941a7eeb06deea
zonble <zonble@gmail.com>
2009-07-26 14:27:29 +0800

    Starts working on the server side API. It parses the web pages of the Central Weather Bureau and convert them to plist or json format.

---
+++ APIServer/main.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#		 http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import wsgiref.handlers
+import weather
+import plistlib
+import json
+
+from google.appengine.ext import webapp
+
+forecast = weather.WeatherForecast()
+
+class ForecastController(webapp.RequestHandler):
+	def get(self):
+		location = self.request.get("location")
+		if len(location) == 0:
+			self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+			locations = forecast.locations()
+			for location in locations:
+				self.response.out.write("<li>")
+				self.response.out.write("<a href=?location=" + str(location['id']) + ">")
+				self.response.out.write(location['location'])
+				self.response.out.write("</a></li>")
+			return
+		items = forecast.fetchWithID(location)
+		if len(items) == 0:
+			return
+		locationName = forecast.locationNameWithID(location)
+
+		outputtype = self.request.get("output")
+		if outputtype == "json":
+			self.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
+			jsonText = json.write({"location":locationName,"forecast":items})
+			self.response.out.write(jsonText)
+			return
+
+		pl = dict(location=locationName, forecast=items)
+		output = plistlib.writePlistToString(pl)
+
+		self.response.headers['Content-Type'] = 'text/plist; charset=utf-8'
+		self.response.out.write(output)
+
+
+class MainHandler(webapp.RequestHandler):
+	def get(self):
+		self.response.out.write('Hello world!')
+
+
+def main():
+	application = webapp.WSGIApplication(
+		[
+			('/', MainHandler),
+			('/forecast', ForecastController)
+			],
+ 			debug=True)
+	wsgiref.handlers.CGIHandler().run(application)
+
+
+if __name__ == '__main__':
+	main()
---
+++ APIServer/weather.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+weather.py
+
+Created by zonble on 2009-07-26.
+"""
+
+import sys
+import os
+import urllib
+import re
+
+WeatherForecastLocations = [
+	{"location": u"台北市", "id": 1},
+	{"location": u"高雄市", "id": 2},
+	{"location": u"基隆", "id": 3},
+	{"location": u"台北", "id": 4},
+	{"location": u"桃園", "id": 5},
+	{"location": u"新竹", "id": 6},
+	{"location": u"苗栗", "id": 7},
+	{"location": u"台中", "id": 8},
+	{"location": u"彰化", "id": 9},
+	{"location": u"南投", "id": 10},
+	{"location": u"雲林", "id": 11},
+	{"location": u"嘉義", "id": 12},
+	{"location": u"台南", "id": 13},
+	{"location": u"高雄", "id": 14},
+	{"location": u"屏東", "id": 15},
+	{"location": u"恆春", "id": 16},
+	{"location": u"宜蘭", "id": 17},
+	{"location": u"花蓮", "id": 18},
+	{"location": u"台東", "id": 19},
+	{"location": u"澎湖", "id": 20},
+	{"location": u"金門", "id": 21},
+	{"location": u"馬祖", "id": 22}
+	]
+
+class WeatherForecast(object):
+	def __init__(self):
+		pass
+	def locations(self):
+		return WeatherForecastLocations
+	def locationNameWithID(self, id):
+		try:
+			if int(id) > len(WeatherForecastLocations):
+				return ""
+		except:
+			return ""
+		for location in WeatherForecastLocations:
+			locationID = location['id']
+			if int(id) == int(locationID):
+				return location['location']
+		return ""
+	def fetchWithID(self, id):
+		try:
+			if int(id) > len(WeatherForecastLocations):
+				return []
+		except:
+			return []
+
+		id = str(id)
+		if len(id) < 2:
+			id = "0" + id
+		urlString = "http://www.cwb.gov.tw/mobile/forecast/city_" + id + ".wml"
+		url = urllib.urlopen(urlString)
+		# print url.read()
+
+		lines = url.readlines()
+		items = []
+
+		title = ""
+		rain = ""
+		temperature = ""
+		description = ""
+		time = ""
+		
+		isHandlingTime = False
+		isHandlingDescription = False
+		
+		for line in lines:
+			if line.startswith("今日白天"):
+				title = u"今日白天"
+				isHandlingTime = True
+			elif line.startswith("今晚至明晨"):
+				title = u"今晚至明晨"
+				isHandlingTime = True
+			elif line.startswith("明日白天"):
+				title = u"明日白天"
+				isHandlingTime = True
+			elif line.startswith("明晚後天"):
+				title = u"明晚後天"
+				isHandlingTime = True
+			elif line.startswith("降雨機率："):
+				line = line[15:-7]
+				rain = line
+				item = {"title":title, "time":time, "description":description, "temperature":temperature, "rain":rain}
+				items.append(item)
+			elif line.startswith("溫度(℃)："):
+				line = line[14: -7]
+				temperature = line
+			elif isHandlingTime == True:
+				time = line[0:-7]
+				isHandlingTime = False
+				isHandlingDescription = True
+			elif isHandlingDescription == True:
+				description = line[0:-7]
+				description = description.decode("utf-8")
+				isHandlingDescription = False
+		self.items = items
+		return items
+	def items(self):
+		return self.items
+
+WeatherOverViewURL = "http://www.cwb.gov.tw/mobile/real.wml"
+
+class WeatherOverview(object):
+	def __init__(self):
+		pass
+	def fetch(self):
+		url = urllib.urlopen(WeatherOverViewURL)
+		lines = url.readlines()
+		count = 0
+		for line in lines:
+			if count == 11:
+				html = re.sub("　　", "</p>\n<p>", line) + "</p>"
+				plain = re.sub("<p>", "", html)
+				plain = re.sub("</p>", "\n", plain)
+				self.html = html
+				self.plain = plain
+			count += 1
+		pass
+	def html(self):
+		return self.html
+	def plain(self):
+		return self.plain
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
